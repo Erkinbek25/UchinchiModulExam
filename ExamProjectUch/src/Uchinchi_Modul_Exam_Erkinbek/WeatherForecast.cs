5bb7b55bd0e07f3604dd27f9afb63c822d100460
Erkinbek25 <erkinbekhasanov1@gmail.com>
2026-03-06 09:26:20 +0500

    tayyorgarlik uchun

---
+++ ExamProjectUch/src/Uchinchi_Modul_Exam_Erkinbek/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Uchinchi_Modul_Exam_Erkinbek
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
